d159b02c1e6c9395e33394f93595d6615273c4b1
nokchax <nokchax@gmail.com>
2020-11-16 23:55:09 +0900

    feat: use before, after all annotation to set up and clean up env

--- src/test/java/com/nokchax/study/nosql/redis/RedisApplicationTests.java
+++ src/test/java/com/nokchax/study/nosql/redis/RedisApplicationTests.java
@@ -3,6 +3,9 @@ package com.nokchax.study.nosql.redis;
 import io.lettuce.core.RedisClient;
 import io.lettuce.core.api.StatefulRedisConnection;
 import io.lettuce.core.api.sync.RedisCommands;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,15 +13,28 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class RedisApplicationTests {
+    private static RedisClient client;
+    private static StatefulRedisConnection<String, String> connection;
+    private static RedisCommands<String, String> command;
+
+    @BeforeAll
+    public static void init() {
+        client = RedisClient.create("redis://localhost:6379/0");
+        connection = client.connect();
+        command = connection.sync();
+    }
+
+    @AfterAll
+    public static void destroy() {
+        connection.close();
+        client.shutdown();
+    }
 
     @Test
+    @DisplayName("레디스 서버에 연결이 잘 되고, 값이 잘 저장되는지")
     void contextLoads() {
         String key = "key";
         String value = "value";
-
-        RedisClient client = RedisClient.create("redis://localhost:6379/0");
-        StatefulRedisConnection<String, String> connection = client.connect();
-        RedisCommands<String, String> command = connection.sync();
 
         // before set
         assertThat(command.get(key)).isNull();
@@ -27,9 +43,6 @@ class RedisApplicationTests {
 
         // after set
         assertThat(command.get(key)).isEqualTo(value);
-
-        connection.close();
-        client.shutdown();
     }
 
 }
