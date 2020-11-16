885018dd8d72a9767af5c042695fdca95cb39e21
nokchax <nokchax@gmail.com>
2020-11-16 23:43:15 +0900

    chore: optimize import

--- src/main/java/com/nokchax/study/nosql/redis/RedisConfig.java
+++ src/main/java/com/nokchax/study/nosql/redis/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.nokchax.study.nosql.redis;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import redis.embedded.RedisServer;
